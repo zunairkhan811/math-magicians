b45940c3b798f09e75cee286a054aabf51dfac68
Muhammad Zunair khan <zunairkhan811@gmail.com>
2023-06-04 02:30:58 +0500

    Add responsiveness to whole website

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -9,31 +9,34 @@ export default function Calculator() {
   };
   return (
     <>
-      <div className="calculator-heading">
-        <h1>Let&apos;s Do Some Math!</h1>
+      <div className="calculator-container">
+        <div className="calculator-heading">
+          <h1>Let&apos;s Do Some Math!</h1>
+        </div>
+        <div className="grid-container">
+          <Result result={output} />
+          <button onClick={handleClick} type="button" className="item">AC</button>
+          <button onClick={handleClick} type="button" className="item">+/-</button>
+          <button onClick={handleClick} type="button" className="item">%</button>
+          <button onClick={handleClick} type="button" className="item color">÷</button>
+          <button onClick={handleClick} type="button" className="item">7</button>
+          <button onClick={handleClick} type="button" className="item">8</button>
+          <button onClick={handleClick} type="button" className="item">9</button>
+          <button onClick={handleClick} type="button" className="item color">x</button>
+          <button onClick={handleClick} type="button" className="item">4</button>
+          <button onClick={handleClick} type="button" className="item">5</button>
+          <button onClick={handleClick} type="button" className="item">6</button>
+          <button onClick={handleClick} type="button" className="item color">-</button>
+          <button onClick={handleClick} type="button" className="item">1</button>
+          <button onClick={handleClick} type="button" className="item">2</button>
+          <button onClick={handleClick} type="button" className="item">3</button>
+          <button onClick={handleClick} type="button" className="item color">+</button>
+          <button onClick={handleClick} type="button" className="item span2">0</button>
+          <button onClick={handleClick} type="button" className="item">.</button>
+          <button onClick={handleClick} type="button" className="item color">=</button>
+        </div>
       </div>
-      <div className="grid-container">
-        <Result result={output} />
-        <button onClick={handleClick} type="button" className="item">AC</button>
-        <button onClick={handleClick} type="button" className="item">+/-</button>
-        <button onClick={handleClick} type="button" className="item">%</button>
-        <button onClick={handleClick} type="button" className="item color">÷</button>
-        <button onClick={handleClick} type="button" className="item">7</button>
-        <button onClick={handleClick} type="button" className="item">8</button>
-        <button onClick={handleClick} type="button" className="item">9</button>
-        <button onClick={handleClick} type="button" className="item color">x</button>
-        <button onClick={handleClick} type="button" className="item">4</button>
-        <button onClick={handleClick} type="button" className="item">5</button>
-        <button onClick={handleClick} type="button" className="item">6</button>
-        <button onClick={handleClick} type="button" className="item color">-</button>
-        <button onClick={handleClick} type="button" className="item">1</button>
-        <button onClick={handleClick} type="button" className="item">2</button>
-        <button onClick={handleClick} type="button" className="item">3</button>
-        <button onClick={handleClick} type="button" className="item color">+</button>
-        <button onClick={handleClick} type="button" className="item span2">0</button>
-        <button onClick={handleClick} type="button" className="item">.</button>
-        <button onClick={handleClick} type="button" className="item color">=</button>
-      </div>
+
     </>
   );
 }
